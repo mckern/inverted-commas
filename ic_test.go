868722ba1699f2e8004546577f80dcf4dd4621a6
Ryan McKern <ryan@orangefort.com>
2017-08-25 12:47:59 -0700

    Test against string literals
    One more layer of validation to ensure that smart quotes are being
    detected correctly by the regexes in use.

--- ic_test.go
+++ ic_test.go
@@ -4,19 +4,27 @@ import "testing"
 
 func TestDoubleSmartQuotes(t *testing.T) {
 	doubleQuoteStr := "\u201c, \u201d, \u201e, \u201f, \u2033, \u2036"
+	doubleQuoteLiterals := `“, ”, „, ‟, ″, ‶`
+
 	actualDoubleResult := doubleQuotes.ReplaceAllString(doubleQuoteStr, "x")
+	actualLiteralResult := doubleQuotes.ReplaceAllString(doubleQuoteLiterals, "x")
 	expectedDoubleResult := "x, x, x, x, x, x"
 
-	if actualDoubleResult != expectedDoubleResult {
+	if (actualDoubleResult != actualLiteralResult) ||
+		(actualDoubleResult != expectedDoubleResult) {
 		t.Fatalf("Expected \"%s\" but got \"%s\"", expectedDoubleResult, actualDoubleResult)
 	}
 }
 func TestSingleSmartQuotes(t *testing.T) {
-	singleQuoteStr := "\u2018, \u2019, \u201a, \u201b, \u2032, \u2035"
-	actualSingleResult := singleQuotes.ReplaceAllString(singleQuoteStr, "x")
-	expectedSingleResult := "x, x, x, x, x, x"
+	singleQuoteStr := "\u2018 \u2019 \u201a \u201b \u2032 \u2035"
+	singleQuoteLiterals := `‘ ’ ‚ ‛ ′ ‵`
 
-	if actualSingleResult != expectedSingleResult {
+	actualSingleResult := singleQuotes.ReplaceAllString(singleQuoteStr, "x")
+	actualLiteralResult := singleQuotes.ReplaceAllString(singleQuoteLiterals, "x")
+	expectedSingleResult := "x x x x x x"
+
+	if (actualSingleResult != actualLiteralResult) ||
+		(actualSingleResult != expectedSingleResult) {
 		t.Fatalf("Expected \"%s\" but got \"%s\"", expectedSingleResult, actualSingleResult)
 	}
 }
